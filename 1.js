d644acf2ac9c968afa782e0577789de8a5930375
laowangbuhsiren <37523372+laowangbuhsiren@users.noreply.github.com>
2018-04-24 07:04:39 +0000

    Delete 1.js

--- 1.js
+++
@@ -1,8 +0,0 @@
-function showpic(whichpic){
-var source=whichpic.getAttribute("href");
-var p=document.getElementById("p");
-p.setAttribute("src",source);
-var text=whichpic.getAttribute("title");
-var d=document.getElementById("d");
-d.firstChild.nodeValue=text;
-}
